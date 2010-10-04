4edf19727b5ebefd3f891e6f0310d36c79feaece
jsor <jan@sorgalla.com>
2010-10-04 14:12:45 +0200

    Add li3 integration

---
+++ integration/lithium/BundleFu.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Du
+ *
+ * LICENSE
+ *
+ * This source file is subject to the BSD license that is available
+ * through the world-wide-web at this URL:
+ * http://opensource.org/licenses/bsd-license.php
+ *
+ * @category   Du
+ * @package    Du_BundleFu
+ * @subpackage Integration
+ * @copyright  Copyright (C) 2010 - Present, Jan Sorgalla
+ * @license    BSD License {@link http://www.opensource.org/licenses/bsd-license.php}
+ */
+
+namespace app\extensions\helper;
+
+/**
+ * li3_bundlefu\extensions\helper\BundleFu
+ *
+ * @category   Du
+ * @package    Du_BundleFu
+ * @subpackage Integration
+ * @author     Jan Sorgalla
+ * @copyright  Copyright (C) 2010 - Present, Jan Sorgalla
+ * @license    BSD License {@link http://www.opensource.org/licenses/bsd-license.php}
+ */
+class BundleFu extends \lithium\template\Helper
+{
+	/**
+     * @var \Du\BundleFu\BundleFu
+     */
+    protected $_bundleFu;
+
+    /**
+	 * Setup autloading for Du namespace.
+	 *
+	 * @return void
+	 */
+	protected function _init() {
+		parent::_init();
+
+		// Setup autoloading
+        spl_autoload_register(function($className) {
+            if (strpos($className, 'Du\\') !== 0) {
+                return;
+            }
+            require str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+            return true;
+        });
+	}
+
+    /**
+     * Set the BundleFu instance
+     *
+     * @param \Du\BundleFu\BundleFu $bundleFu
+     * @return Zend_View_Helper_BundleFu
+     */
+    public function setBundleFu(\Du\BundleFu\BundleFu $bundleFu)
+    {
+        $this->_bundleFu = $bundleFu;
+        return $this;
+    }
+
+    /**
+     * Get the BundleFu instance
+     *
+     * @return \Du\BundleFu\BundleFu
+     */
+    public function getBundleFu()
+    {
+        if (null === $this->_bundleFu) {
+            $this->_bundleFu = new \Du\BundleFu\BundleFu();
+            $this->_bundleFu->setDocRoot($this->_context->request()->env('DOCUMENT_ROOT'));
+        }
+
+        return $this->_bundleFu;
+    }
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     */
+    public function __call($method, $params)
+    {
+        $return = call_user_func_array(array($this->getBundleFu(), $method), $params);
+
+        switch ($method) {
+            case 'start':
+            case 'end':
+            case substr($method, 0, 3) == 'set':
+                return $this;
+            default:
+                return $return;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        try {
+            $return = $this->getBundleFu()->render();
+            return $return;
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_WARNING);
+            return '';
+        }
+    }
+}
+
+?>
---
+++ integration/lithium/README.markdown
@@ -0,0 +1,25 @@
+Lithium Integration
+================================
+
+BundleFu.php is a Lithium view helper integrating Du_BundeFu into your Lithium applications.
+
+## Installation ##
+
+Place BundleFu.php in app/extensions/helper.
+
+## Usage ##
+
+Use the helper in your views:
+
+    <?php $this->bundleFu->start(); ?>
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript" src="/js/jquery.myplugin.js"></script>
+    <script type="text/javascript" src="/js/app.js"></script>
+    <script type="text/javascript" src="/js/app.module.js"></script>
+    <link media="screen" type="text/css" href="/css/reset.css">
+    <link media="screen" type="text/css" href="/css/jquery.myplugin.css">
+    <link media="screen" type="text/css" href="/css/app.css">
+    <link media="screen" type="text/css" href="/css/app.module.css">
+    <?php $this->bundleFu->end(); ?>
+
+    <?php echo $this->bundleFu; ?>
