c9956d6dd2fb4f69a1ec3190699269ba9f72071e
Jan Sorgalla <jsorgalla@gmail.com>
2015-02-13 15:50:12 +0100

    Update copyright year

--- tests/DotsUnited/BundleFu/Tests/Filter/ClosureCompilerServiceFilterTest.php
+++ tests/DotsUnited/BundleFu/Tests/Filter/ClosureCompilerServiceFilterTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of BundleFu.
  *
- * (c) 2013 Jan Sorgalla <jan.sorgalla@dotsunited.de>
+ * (c) 2015 Jan Sorgalla <jan.sorgalla@dotsunited.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
